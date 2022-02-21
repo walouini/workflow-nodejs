70e204223a509d7c0ad7818bb67dd466d8d46975
walid <walid.alouini1@gmail.com>
2022-02-21 16:03:32 +0100

    add controller and routes files

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+userController = require("../controlleur/user-controller");
+const cors = require('cors');
+
+module.exports = (route) =>{
+
+    route.get("/users", cors(),userController.readAll);
+
+    route.get("/user/:id", cors(),userController.read);
+    
+    route.post('/user', cors(),userController.create);
+    
+};
